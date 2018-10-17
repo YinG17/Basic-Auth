3843981ae5e7ecafc59bf0bfc1beffcc0fb477cc
JAEHO SONG <thruthesky@gmail.com>
2018-10-17 19:17:42 +0800

    update for user register, post create

--- basic-auth.php
+++ basic-auth.php
@@ -104,9 +104,24 @@ add_filter( 'rest_authentication_errors', 'json_basic_auth_error' );
  * @return array
  */
 function give_permissions( $allcaps, $cap, $args ) {
-                        //    $allcaps[$cap[0]] = true; // This is wrong. 이렇게 하면, 모든 요청되는 권한을 주므로 안된다.
-    $allcaps['create_users'] = true; // Allow only creating user.
-                        //    $allcaps['rest_cannot_create'] = true; // This is not working. 이것을 해도 사용자가 Rest Api 로 글 작성 할 수 없음. 그래서 아래와 같이 회원 가입하면 권한을 줌.
+                                //    $allcaps['rest_cannot_create'] = true; // This is not working. 이것을 해도 사용자가 Rest Api 로 글 작성 할 수 없음. 그래서 아래와 같이 회원 가입하면 권한을 줌.
+                                //    $allcaps[$cap[0]] = true; // This is wrong. 이렇게 하면, 모든 요청되는 권한을 주므로 안된다.
+
+    /**
+     * Allow anyone can register
+     */
+    $allcaps['create_users'] = true;
+
+    /**
+     * Allow user update only his user data. Not others.
+     * $args[1] is login user's ID.
+     * $args[2] is the user ID that will be updated.
+     */
+    if ( $args[1] == $args[2] ) {
+        $allcaps['edit_users'] = true;
+    }
+
+
     return $allcaps;
 }
 add_filter( 'user_has_cap', 'give_permissions', 10, 3 );
@@ -127,12 +142,14 @@ add_action( 'user_register', 'do_user_register', 10, 1 );
 
 /**
  * It returns user's security code every 'user' Rest Api call.
+ * @note When user is not logged in(Especially when user has registered), it compares if the user is the last user who registered to wordpress,
+ *          If the user is the last user ( Meaning, just registered, returns security code also.
  * It only returns if my user information is requested. Meaning security code for others will not be returned.
  */
 register_rest_field( 'user', 'security_code',
     array(
         'get_callback'    => function ( $user ) {
-            if ( $user['id'] == wp_get_current_user()->ID ) {
+            if ( $user['id'] == wp_get_current_user()->ID || $user['id'] == get_last_user_ID() ) {
                 return get_security_code( $user['id'] );
             }
         },
@@ -140,6 +157,19 @@ register_rest_field( 'user', 'security_code',
         'schema'          => null,
     )
 );
+//register_rest_field( 'user', 'whatever',
+//    array(
+//        'get_callback'    => function ( $user ) {
+//            if ( $user['id'] == wp_get_current_user()->ID || $user['id'] == get_last_user_ID() ) {
+//                $u = get_userdata($user['id']);
+//                return $u->name;
+//            }
+//        },
+//        'update_callback' => null,
+//        'schema'          => null,
+//    )
+//);
+
 
 
 /**
@@ -153,3 +183,51 @@ function get_security_code( $ID ) {
     $security_source = "{$user->ID},{$user->user_email},{$user->user_registered},{$user->user_pass}";
     return md5($security_source);
 }
+
+
+function get_last_user_ID() {
+    $args = array(
+//        'role'         => 'author', // authors only
+        'orderby'      => 'registered', // registered date
+        'order'        => 'DESC', // last registered goes first
+        'number'       => 1 // limit to the last one, not required
+    );
+
+    $users = get_users( $args );
+
+    $last_user_registered = $users[0]; // the first user from the list
+
+    return $last_user_registered->ID;
+}
+
+
+/**
+ * Returns login user's data.
+ */
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'custom/api', '/profile', array(
+        'methods' => 'GET',
+        'callback' => function() {
+
+            if ( is_user_logged_in() ) {
+                $user = wp_get_current_user();
+                $re = [
+                    'email' => $user->user_email,
+                    'id' => $user->ID,
+                    'first_name' => $user->first_name,
+                    'last_name' => $user->last_name,
+                    'name' => $user->display_name,
+                    'nickname' => $user->nickname,
+                    'meta' => '',
+                    'register_date' => $user->user_registered,
+                    'username' => $user->user_login,
+                    'security_code' => get_security_code($user->ID)
+                ];
+
+                return $re;
+            } else {
+                return null;
+            }
+        },
+    ) );
+} );
